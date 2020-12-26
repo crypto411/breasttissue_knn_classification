23b1c1e80588b30e92b000aa4e158f3ab02aa514
Fadhlan Hadaina <44132966+crypto411@users.noreply.github.com>
2020-12-26 19:28:42 +0700

    Add files via upload

---
+++ breasttissue_predict.py
@@ -0,0 +1,431 @@
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(472, 284)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(40, 60, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_10 = QtWidgets.QLabel(Dialog)
+        self.label_10.setGeometry(QtCore.QRect(110, 20, 281, 31))
+        font = QtGui.QFont()
+        font.setFamily("Kristen ITC")
+        font.setPointSize(14)
+        self.label_10.setFont(font)
+        self.label_10.setObjectName("label_10")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(40, 90, 131, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(40, 120, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(40, 150, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(40, 180, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(40, 210, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(Dialog)
+        self.label_7.setGeometry(QtCore.QRect(170, 60, 16, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(170, 90, 16, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_8.setFont(font)
+        self.label_8.setObjectName("label_8")
+        self.label_9 = QtWidgets.QLabel(Dialog)
+        self.label_9.setGeometry(QtCore.QRect(170, 120, 16, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_9.setFont(font)
+        self.label_9.setObjectName("label_9")
+        self.label_11 = QtWidgets.QLabel(Dialog)
+        self.label_11.setGeometry(QtCore.QRect(170, 150, 16, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_11.setFont(font)
+        self.label_11.setObjectName("label_11")
+        self.label_12 = QtWidgets.QLabel(Dialog)
+        self.label_12.setGeometry(QtCore.QRect(170, 180, 16, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_12.setFont(font)
+        self.label_12.setObjectName("label_12")
+        self.label_13 = QtWidgets.QLabel(Dialog)
+        self.label_13.setGeometry(QtCore.QRect(170, 210, 16, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_13.setFont(font)
+        self.label_13.setObjectName("label_13")
+        self.label_14 = QtWidgets.QLabel(Dialog)
+        self.label_14.setGeometry(QtCore.QRect(190, 60, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_14.setFont(font)
+        self.label_14.setObjectName("label_14")
+        self.label_15 = QtWidgets.QLabel(Dialog)
+        self.label_15.setGeometry(QtCore.QRect(190, 90, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_15.setFont(font)
+        self.label_15.setObjectName("label_15")
+        self.label_16 = QtWidgets.QLabel(Dialog)
+        self.label_16.setGeometry(QtCore.QRect(190, 120, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_16.setFont(font)
+        self.label_16.setObjectName("label_16")
+        self.label_17 = QtWidgets.QLabel(Dialog)
+        self.label_17.setGeometry(QtCore.QRect(190, 150, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_17.setFont(font)
+        self.label_17.setObjectName("label_17")
+        self.label_18 = QtWidgets.QLabel(Dialog)
+        self.label_18.setGeometry(QtCore.QRect(190, 180, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_18.setFont(font)
+        self.label_18.setObjectName("label_18")
+        self.label_19 = QtWidgets.QLabel(Dialog)
+        self.label_19.setGeometry(QtCore.QRect(190, 210, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_19.setFont(font)
+        self.label_19.setObjectName("label_19")
+        self.label_20 = QtWidgets.QLabel(Dialog)
+        self.label_20.setGeometry(QtCore.QRect(320, 90, 131, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_20.setFont(font)
+        self.label_20.setObjectName("label_20")
+        self.label_21 = QtWidgets.QLabel(Dialog)
+        self.label_21.setGeometry(QtCore.QRect(300, 120, 151, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_21.setFont(font)
+        self.label_21.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_21.setObjectName("label_21")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Carcinoma"))
+        self.label_10.setText(_translate("Dialog", "Hasil Prediksi Breast Tissue"))
+        self.label_2.setText(_translate("Dialog", "Fibro-adenoma"))
+        self.label_3.setText(_translate("Dialog", "Mastopathy"))
+        self.label_4.setText(_translate("Dialog", "Glandular"))
+        self.label_5.setText(_translate("Dialog", "Connective"))
+        self.label_6.setText(_translate("Dialog", "Adipose"))
+        self.label_7.setText(_translate("Dialog", ":"))
+        self.label_8.setText(_translate("Dialog", ":"))
+        self.label_9.setText(_translate("Dialog", ":"))
+        self.label_11.setText(_translate("Dialog", ":"))
+        self.label_12.setText(_translate("Dialog", ":"))
+        self.label_13.setText(_translate("Dialog", ":"))
+        self.label_14.setText(_translate("Dialog", "0.0 %"))
+        self.label_15.setText(_translate("Dialog", "0.0 %"))
+        self.label_16.setText(_translate("Dialog", "0.0 %"))
+        self.label_17.setText(_translate("Dialog", "0.0 %"))
+        self.label_18.setText(_translate("Dialog", "0.0 %"))
+        self.label_19.setText(_translate("Dialog", "0.0 %"))
+        self.label_20.setText(_translate("Dialog", "Classnya ialah"))
+        self.label_21.setText(_translate("Dialog", "Fibro-adenom"))
+    
+    def updateUi(self, prob, result):
+        print(prob, result)
+        _translate = QtCore.QCoreApplication.translate
+        self.label_14.setText(_translate("Dialog", str(prob[0][1] * 100)) + " %") # Car
+        self.label_15.setText(_translate("Dialog", str(prob[0][3] * 100)) + " %") # Fad
+        self.label_16.setText(_translate("Dialog", str(prob[0][5] * 100)) + " %") # Mas
+        self.label_17.setText(_translate("Dialog", str(prob[0][4] * 100)) + " %") # Gla
+        self.label_18.setText(_translate("Dialog", str(prob[0][2] * 100)) + " %") # Con
+        self.label_19.setText(_translate("Dialog", str(prob[0][0] * 100)) + " %") # Adi
+        classResult = {"car": "Carcinoma", "fad": "Fibro-adenoma", "mas": "Mastopathy", "gla": "Glandular", "con": "Connective", "adi": "Adipose"}
+        self.label_21.setText(_translate("Dialog", classResult[result[0]]))
+
+def breasttissue_predict(input, of):
+    # pandas library dataframe
+    import pandas as pd
+
+    # numpy untuk array, range number, algabar linear, dll
+    import numpy as np
+
+    # visualisasi grafik
+    import matplotlib.pyplot as plt
+
+    # library untuk KNN
+    from sklearn.neighbors import KNeighborsClassifier
+
+    # untuk split data train dan test
+    from sklearn.model_selection import train_test_split
+
+    # read data csv ke data frame
+    df = pd.read_csv('breasttissue.csv')
+
+    # menghilangkan kolom case
+    df = df.drop(columns=['Case #'])
+
+    # membagi data terikat (y) dan data bebas (x)
+    x = df.loc[:, df.columns != 'Class']
+    y = df['Class']
+
+    # variabel train test, test menjadi 20% / 0.2
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    # neighbours array dari 1 sampai 9
+    neighbors = np.arange(1,9)
+
+    # membuat array kosong dengan panjang 9
+    train_accuracy =np.empty(len(neighbors))
+    test_accuracy = np.empty(len(neighbors))
+
+    # for untuk looping panjang dari neighbours
+    for i,k in enumerate(neighbors):
+        #Setup a knn classifier with k neighbors
+        knn = KNeighborsClassifier(n_neighbors=k)
+        #Fit the model
+        knn.fit(X_train, y_train)
+        
+        #Compute accuracy on the training set
+        train_accuracy[i] = knn.score(X_train, y_train)
+        
+        #Compute accuracy on the test set
+        test_accuracy[i] = knn.score(X_test, y_test) 
+
+    # Membuat grafik
+    plt.title('k-NN Varying number of neighbors')
+    plt.plot(neighbors, test_accuracy, label='Testing Accuracy')
+    plt.plot(neighbors, train_accuracy, label='Training accuracy')
+    plt.legend()
+    plt.xlabel('Number of neighbors')
+    plt.ylabel('Accuracy')
+    # plt.show()
+
+    # menginisiasi objek dengan hyperparameter 4
+    knn = KNeighborsClassifier(n_neighbors=4)
+        
+    # meng train algoritma knn hyperparameter 4
+    knn.fit(X_train, y_train)
+    knn.score(X_test,y_test)
+
+    x.columns.values
+    print(dict.fromkeys(y))
+    # input variabel perumpamaan
+    # input  = [[0, 1.1, 10, 0, 1.1, 1.1, 1.1, 1.1, 200]]
+    # ['I0', 'PA500', 'HFS', 'DA', 'Area', 'A/DA', 'Max IP', 'DR', 'P']
+    # input  = [[0.3 for i in range(9)]]
+
+    # Hasil dari keyakinan model
+    np.nanmax(knn.predict_proba(input))    
+
+    predict_proba   = knn.predict_proba(input)
+    predict         = knn.predict(input)
+    print(predict_proba)
+    print(predict)
+
+    # df.describe()
+    of.updateUi(predict_proba, predict)
+    # app.exec_()
+
+class Ui_MainWindow(object):
+    def __init__(self, dialog, of):
+        self.dialog = dialog
+        self.of = of
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(372, 455)
+        MainWindow.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        MainWindow.setMouseTracking(True)
+        MainWindow.setAutoFillBackground(True)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 70, 21, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(40, 110, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(40, 150, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(40, 190, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(40, 230, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(40, 270, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(40, 310, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(40, 350, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_8.setFont(font)
+        self.label_8.setObjectName("label_8")
+        self.label_9 = QtWidgets.QLabel(self.centralwidget)
+        self.label_9.setGeometry(QtCore.QRect(40, 390, 51, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_9.setFont(font)
+        self.label_9.setObjectName("label_9")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(260, 210, 75, 23))
+        self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButton.setAutoFillBackground(True)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.predict_clicked)
+        self.label_10 = QtWidgets.QLabel(self.centralwidget)
+        self.label_10.setGeometry(QtCore.QRect(100, 10, 161, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_10.setFont(font)
+        self.label_10.setObjectName("label_10")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(100, 70, 113, 31))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(100, 110, 113, 31))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(100, 150, 113, 31))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(100, 190, 113, 31))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_5.setGeometry(QtCore.QRect(100, 230, 113, 31))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_6.setGeometry(QtCore.QRect(100, 270, 113, 31))
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_7.setGeometry(QtCore.QRect(100, 310, 113, 31))
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.lineEdit_8 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_8.setGeometry(QtCore.QRect(100, 350, 113, 31))
+        self.lineEdit_8.setObjectName("lineEdit_8")
+        self.lineEdit_9 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_9.setGeometry(QtCore.QRect(100, 390, 113, 31))
+        self.lineEdit_9.setObjectName("lineEdit_9")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Input Data"))
+        self.label.setText(_translate("MainWindow", "I0"))
+        self.label_2.setText(_translate("MainWindow", "PA500"))
+        self.label_3.setText(_translate("MainWindow", "HFS"))
+        self.label_4.setText(_translate("MainWindow", "DA"))
+        self.label_5.setText(_translate("MainWindow", "Area"))
+        self.label_6.setText(_translate("MainWindow", "A/DA"))
+        self.label_7.setText(_translate("MainWindow", "Max IP"))
+        self.label_8.setText(_translate("MainWindow", "DR"))
+        self.label_9.setText(_translate("MainWindow", "P"))
+        self.pushButton.setText(_translate("MainWindow", "Prediksi"))
+        self.label_10.setText(_translate("MainWindow", "Prediksi Breast Tissue"))
+        self.lineEdit.setText(_translate("MainWindow", "0"))
+        self.lineEdit_2.setText(_translate("MainWindow", "0"))
+        self.lineEdit_3.setText(_translate("MainWindow", "0"))
+        self.lineEdit_4.setText(_translate("MainWindow", "0"))
+        self.lineEdit_5.setText(_translate("MainWindow", "0"))
+        self.lineEdit_6.setText(_translate("MainWindow", "0"))
+        self.lineEdit_7.setText(_translate("MainWindow", "0"))
+        self.lineEdit_8.setText(_translate("MainWindow", "0"))
+        self.lineEdit_9.setText(_translate("MainWindow", "0"))
+
+    def predict_clicked(self):
+        form_data = list([[int(self.lineEdit.text()), int(self.lineEdit_2.text()), int(self.lineEdit_3.text()), int(self.lineEdit_4.text()), int(self.lineEdit_5.text()), int(self.lineEdit_6.text()), int(self.lineEdit_7.text()), int(self.lineEdit_8.text()), int(self.lineEdit_9.text())]])
+        breasttissue_predict(form_data, of)
+        self.dialog.show()
+
+if __name__ == "__main__":
+    import sys
+    from PyQt5 import QtCore, QtGui, QtWidgets
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    of = Ui_Dialog()
+    of.setupUi(Dialog)
+
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow(Dialog, of)
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+
+    sys.exit(app.exec_())
